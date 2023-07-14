732d80e3accf329fa87f18f1be24c51edba12663
MD.  Shanto <135007992+shanto0011@users.noreply.github.com>
2023-07-14 20:47:36 +0600

    Add files via upload

---
+++ Basic Coding/C++/sub_array.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void printSubarray(vector<int> arr , int n){
+
+
+    for(int i = 0 ; i < n ; i++){
+        for(int j = i ; j < n ; j++){
+            for(int k = i; k <= j; k++){
+                cout << arr[k] << ",";
+            }
+            cout << endl;
+        }
+    }
+}
+
+int main(){
+    vector<int> arr = {10, 20, 30, 40, 50, 60};
+
+    int n = arr.size();
+
+    printSubarray(arr,n);
+
+    return 0;
+}
